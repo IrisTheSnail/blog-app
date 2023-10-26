534e8c7e7a2d8ad1c872cd11b29caf0db67465db
IrisTheSnail <salwa999adlouni@gmail.com>
2023-10-26 16:47:08 +0100

    [frontend] frontend initialisation

--- backend/utils/JWT.ts
+++ backend/utils/JWT.ts
@@ -5,8 +5,10 @@ import jwt from "jsonwebtoken";
 
 export const JWTgenerator = (username : string, email : string) => {
     try {
-        const payload = { username, email };
-        console.log(env.default.JWT_SECRET);
+        const payload : {username : string, email : string} = { username, email };
+        const secret : jwt.Secret = env.default.JWT_SECRET;
+        const options : jwt.SignOptions = {expiresIn: 3600, algorithm : "HS256"};
+        console.log(secret);
         /**
          *  export function sign(
             payload: string | Buffer | object,
@@ -14,12 +16,11 @@ export const JWTgenerator = (username : string, email : string) => {
             options?: SignOptions,
         ): string;
          */
-        const token : string = jwt.sign(payload, env.default.JWT_SECRET, 
-            {expiresIn: 3600, algorithm : "HS256"});
+        const token : string = jwt.sign(payload, secret, options);
 
         return { error: false, token };
     } catch (error) {
-        console.log(error);
+        console.error();
         return { error: true };
     }
 }
